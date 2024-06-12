e1543b2da46da72579f9901fa3442adc1337b4d1
asim shah <asimshah2111@gmail.com>
2024-06-13 03:09:29 +0530

    code rebasing done

--- chaincode/lib/assetTransfer.js
+++ chaincode/lib/assetTransfer.js
@@ -1,17 +1,13 @@
-/*
- * Copyright IBM Corp. All Rights Reserved.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 'use strict';
-
-// Deterministic JSON.stringify()
 const stringify  = require('json-stringify-deterministic');
 const sortKeysRecursive  = require('sort-keys-recursive');
 const { Contract } = require('fabric-contract-api');
 
 class AssetTransfer extends Contract {
+
+
+    //start writing your contract functions from here
+
     async addNumbers(ctx ,num1, num2) {
         console.log("called");
         const result = parseInt(num1) + parseInt(num2);
