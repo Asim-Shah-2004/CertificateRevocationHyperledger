dc39c2c35668724a21da4a83c8fd860a71bd51cd
asim shah <asimshah2111@gmail.com>
2024-06-26 21:02:45 +0530

    added update route

--- application/controllers/ipfsControllers/updateController.js
+++ application/controllers/ipfsControllers/updateController.js
@@ -2,27 +2,42 @@ import getBlock from "../../services/hyperledgerServices/getBlockService.js";
 import { getIpfsInstance } from "../../services/ipfsServices/ipfsClientService.js";
 import { dagJson } from "@helia/dag-json";
 import { CID } from "multiformats/cid";
+
 const helia = await getIpfsInstance();
 const dag = dagJson(helia);
 
-const updateIPFS = async(req,res)=>{
-    const {ABCID} = req.body
-    try{
-        try{
-            const blockHash = await getBlock(ABCID)
-            if(blockHash==null) return res.send({"message" : "entry does not exists"})
-            const cid = CID.parse(blockHash);
-            const data = await dag.get(cid)
-            console.log(data);
-            
-            return res.send({"data":data})
-        }catch(err){
-            console.log(err);
-        }
-    }catch(err){
-        console.log(err);
+const updateIPFS = async (req, res) => {
+  const { ABCID, permissions, credits, last_added, marks, certificates } = req.body;
+  try {
+    const blockHash = await getBlock(ABCID);
+    if (blockHash == null) {
+      return res.status(404).send({ message: "Entry does not exist" });
     }
-    
-}
 
-export default updateIPFS
+    const cid = CID.parse(blockHash);
+    const data = await dag.get(cid);
+    
+
+    console.log(data);    
+    if (!data) {
+      return res.status(500).json({ message: "Failed to retrieve data from IPFS" });
+    }
+
+    
+    data.permissions = permissions || data.permissions;
+    data.credits = credits || data.credits;
+    data.last_added = last_added || data.last_added;
+    data.marks = marks || data.marks;
+    data.certificates = certificates || data.certificates;
+
+    
+    const updatedCid = await dag.add(data);
+
+    res.status(200).json({ message: "Data updated on IPFS", cid: updatedCid.toString() });
+  } catch (err) {
+    console.log("Error updating data on IPFS:", err);
+    res.status(500).json({ message: "Failed to update data on IPFS", error: err.message });
+  }
+};
+
+export default updateIPFS;
