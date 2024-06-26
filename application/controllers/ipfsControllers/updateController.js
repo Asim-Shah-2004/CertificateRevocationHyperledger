b156eb6addd35c5fdb7436914b1a0f282fd6c615
asim shah <asimshah2111@gmail.com>
2024-06-26 19:54:04 +0530

    started working on update route

--- application/controllers/ipfsControllers/updateController.js
+++ application/controllers/ipfsControllers/updateController.js
@@ -1,4 +1,9 @@
 import getBlock from "../../services/hyperledgerServices/getBlockService.js";
+import { getIpfsInstance } from "../../services/ipfsServices/ipfsClientService.js";
+import { dagJson } from "@helia/dag-json";
+import { CID } from "multiformats/cid";
+const helia = await getIpfsInstance();
+const dag = dagJson(helia);
 
 const updateIPFS = async(req,res)=>{
     const {ABCID} = req.body
@@ -6,7 +11,9 @@ const updateIPFS = async(req,res)=>{
         try{
             const blockHash = await getBlock(ABCID)
             if(blockHash==null) return res.send({"message" : "entry does not exists"})
-            res.send({"message":blockHash})
+            const cid = CID.parse(blockHash);
+            const data = await dag.get(cid)
+            return res.send({"data":data})
         }catch(err){
             console.log(err);
         }
